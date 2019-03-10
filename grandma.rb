016f4324028a8d7a8c87b0ae2dd15f9544e925ab
Kevin Malazarte <kevin.malazarte@gmail.com>
2019-03-10 03:20:01 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,5 @@
 # Write a speak_to_grandma method.
 
-def speak_to_grandma(phrase)
-  if phrase == "I LOVE YOU GRANDMA!"
-    print "I LOVE YOU TOO PUMPKIN!"
-end
-
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
